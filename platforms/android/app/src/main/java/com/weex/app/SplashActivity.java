e300ed419f03c0010f31b56a1b9108e7fc942e92
zhouronghua@cdeledu.com <zhouronghua@cdeledu.com>
2018-11-28 15:56:59 +0800

    URL参数传递

--- platforms/android/app/src/main/java/com/weex/app/SplashActivity.java
+++ platforms/android/app/src/main/java/com/weex/app/SplashActivity.java
@@ -14,41 +14,43 @@ import android.view.animation.ScaleAnimation;
 public class SplashActivity extends AppCompatActivity {
 
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_splash);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-    View textView = findViewById(R.id.fullscreen_content);
-    ScaleAnimation scaleAnimation = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-    RotateAnimation rotateAnimation = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        View textView = findViewById(R.id.fullscreen_content);
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        RotateAnimation rotateAnimation = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
 
-    AnimationSet animationSet = new AnimationSet(false);
-    animationSet.addAnimation(scaleAnimation);
-    animationSet.addAnimation(rotateAnimation);
-    animationSet.setDuration(1500);
+        AnimationSet animationSet = new AnimationSet(false);
+        animationSet.addAnimation(scaleAnimation);
+        animationSet.addAnimation(rotateAnimation);
+        animationSet.setDuration(1500);
 
-    animationSet.setAnimationListener(new Animation.AnimationListener() {
-      @Override
-      public void onAnimationStart(Animation animation) {
-      }
+        animationSet.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
 
-      @Override
-      public void onAnimationEnd(Animation animation) {
-        Intent intent = new Intent(SplashActivity.this, WXPageActivity.class);
-        Uri data = getIntent().getData();
-        if (data != null) {
-          intent.setData(data);
-        }
-        intent.putExtra("from", "splash");
-        startActivity(intent);
-        finish();
-      }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(SplashActivity.this, WXPageActivity.class);
+                Uri data = getIntent().getData();
+                if (data != null) {
+                    intent.setData(data);
+                } else {
+                    intent.setData(Uri.parse("http://192.168.190.153:8888/bundle/components/HelloWorld.js"));
+                }
+                intent.putExtra("from", "splash");
+                startActivity(intent);
+                finish();
+            }
 
-      @Override
-      public void onAnimationRepeat(Animation animation) {
-      }
-    });
-    textView.startAnimation(animationSet);
-  }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        textView.startAnimation(animationSet);
+    }
 }
